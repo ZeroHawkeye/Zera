726759301c686e1c0264f291e816ebb0c970625d
Zero <166997982@qq.com>
2025-11-27 18:05:40 +0800

    update docs

--- docs/mdx-components.tsx
+++ docs/mdx-components.tsx
@@ -1,9 +1,19 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
+import { Step, Steps } from 'fumadocs-ui/components/steps';
+import { Tab, Tabs } from 'fumadocs-ui/components/tabs';
+import { File, Folder, Files } from 'fumadocs-ui/components/files';
 import type { MDXComponents } from 'mdx/types';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    Step,
+    Steps,
+    Tab,
+    Tabs,
+    File,
+    Folder,
+    Files,
     ...components,
   };
 }
